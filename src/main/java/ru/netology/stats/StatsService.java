ab9eb5b7342ae08a429b89241d7a17962bf04765
romanmihailow <romanmihailow@email.com>
2023-01-23 19:40:22 +0500

    initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,78 @@
+package ru.netology.stats;
+public class StatsService {
+
+
+
+    // Сумму всех продаж.
+    public int summAllSale() {
+
+
+        //вычисление
+        int summAllSale = 0;
+
+        //функция возвращает
+        return summAllSale;
+    }
+
+
+
+    // Среднюю сумму продаж в месяц.
+    public int averageSummSaleMonth() {
+        //вычисление
+        int averageSummSaleMonth = 0;
+
+
+        //функция возвращает
+        return averageSummSaleMonth;
+    }
+
+
+
+    // Номер месяца, в котором был пик продаж, то есть осуществлены продажи на максимальную сумму*.
+    public int numberMonthMaxSale() {
+        //вычисление
+        int numberMonthMaxSale = 0;
+
+
+        //функция возвращает
+        return numberMonthMaxSale;
+    }
+
+
+
+    // Номер месяца, в котором был минимум продаж, то есть осуществлены продажи на минимальную сумму*.
+    public int numberMonthMinSale() {
+        //вычисление
+        int numberMonthMinSale = 0;
+
+
+
+        //функция возвращает
+        return numberMonthMinSale;
+    }
+
+
+
+    // Количество месяцев, в которых продажи были ниже среднего (см. п.2).
+    public int quantityMonthSaleDownAverege() {
+        //вычисление
+        int quantityMonthSaleMinAverege = 0;
+
+
+        //функция возвращает
+        return quantityMonthSaleMinAverege;
+    }
+
+
+
+    // Количество месяцев, в которых продажи были выше среднего (см. п.2).
+    public int quantityMonthSaleUpAverege() {
+        //вычисление
+        int quantityMonthSaleUpAverege = 0;
+
+
+        //функция возвращает
+        return quantityMonthSaleUpAverege;
+    }
+
+}
